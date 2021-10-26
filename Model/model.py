456e33c97f40708a65042122c963c56cb95bb5c2
zhuzihao <zhuzihao@mediworks.biz>
2021-10-26 16:49:25 +0800

    update Model.model examples and make it more simpler

--- Model/model.py
+++ Model/model.py
@@ -12,32 +12,23 @@ class SimpleModel(BaseModel):
         super(SimpleModel, self).__init__()
 
         self.num_classes = num_classes
+    
         self.input_data = Input(shape=(input_h, input_w, input_c), name="input")
-
-        self.backbone_output = self._make_backbone(self.input_data)
-        self.classifier_output = self._make_clf(self.backbone_output)
-
+        self.output_data = self._make_model(self.input_data)
         self.model = Model(self.input_data, self.classifier_output, name="mymodel")
-
 
     def call(self, input):
         return self.model(input)
 
-
-    def _make_backbone(self, input_data):
+    def _make_model(self, input):
         padding = "same"
-        x = Conv2D(16, kernel_size=(3, 3), strides=1, activation='relu', padding=padding)(input_data)
+        x = Conv2D(16, kernel_size=(3, 3), strides=1, activation='relu', padding=padding)(input)
         x = MaxPooling2D()(x)
         for i in range(1, 4):
             x = Conv2D(16 * (2 ** i), kernel_size=(3, 3), strides=1, activation='relu', padding=padding)(x)
             x = MaxPooling2D()(x)
         x = GlobalMaxPooling2D()(x)
-
-        return x
-
-
-    def _make_clf(self, input_data):
-        x = Dense(128, activation="relu")(input_data)
+        x = Dense(128, activation="relu")(x)
         x = Dense(64, activation="relu")(x)
         predictions = Dense(self.num_classes, activation="sigmoid", name="output")(x)
 
